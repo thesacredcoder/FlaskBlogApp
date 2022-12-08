d3421dd273cdfae5ab2841aa998f3a8defb73e13
thesacredcoder <akashsamal4875@gmail.com>
2022-12-08 09:38:36 +0530

    added flash messages

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "thisissomesupersecretkey"
+
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
@@ -18,3 +27,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('internalError.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+
+    return render_template("name.html", name = name, form = form)
